581df8e59845cd50236e9f09052f90ddbe812996
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2025-03-23 13:25:59 +0530

    Full Project Done

---
+++ src/components/Search.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Search = ({searchTerm,setSearchterm}) => {
+  return (
+    <div className='search'>
+        
+        <div>
+
+            <img src="search.svg" alt="search" />
+            <input 
+            type="text" 
+            placeholder='Search through thousands of movies'
+            value={searchTerm}
+            onChange={(e) => setSearchterm(e.target.value) }
+            />
+
+        </div>
+    </div>
+  )
+}
+
+export default Search
